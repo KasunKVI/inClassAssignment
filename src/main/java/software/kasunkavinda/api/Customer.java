f13400ef369a74669891aa3febe86a91110a554d
KasunKVI <kkhettiarachchi10@gmail.com>
2024-02-03 10:47:57 +0530

    init the project

---
+++ src/main/java/software/kasunkavinda/api/Customer.java
@@ -0,0 +1,19 @@
+package software.kasunkavinda.api;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "Cutomer", urlPatterns = "/customer")
+public class Customer extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
+}
